d1efeb88b85e80fb87627223c9f21746b437e481
lijar <beljar1214@gmail.com>
2022-04-19 12:25:30 +0630

    'add'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import Imageslider from './components/Imageslider'
+import ScrollableTabsButtonForce from './components/category'
 
 function App() {
   return (
     <div className="App">
       <Navbar className="navbar-app" />
       <Imageslider />
+      <ScrollableTabsButtonForce className="category" />
     </div>
   )
 }
