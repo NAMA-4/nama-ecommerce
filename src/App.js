469255108bd05f60cfcfe4aeefcab29252c16af3
lijar <beljar1214@gmail.com>
2022-04-19 11:06:33 +0630

    'Add

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from './components/Navbar'
+import Imageslider from './components/Imageslider'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar className="navbar-app" />
+      <Imageslider />
+    </div>
+  )
+}
+
+export default App
