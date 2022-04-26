4003589b0f3f944e45275851e3bcb93c2e1f62d7
lijar <beljar1214@gmail.com>
2022-04-26 13:50:27 +0630

    'ad'

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,7 @@ const Home = (props) => {
     <div>
       <Imageslider />
       <FoodProductCategory />
+      <RecentlyAdded />
       {/* <ContentProduct productCollection="Products" /> */}
     </div>
   )
@@ -24,6 +25,41 @@ const FoodProductCategory = () => {
         <div className="product">
           <img className="product-img" src="./img/product-logo.png" alt="" />
           <span> ကုန်ပစ္စည်း</span>
+        </div>
+      </div>
+    </>
+  )
+}
+
+const RecentlyAdded = () => {
+  return (
+    <>
+      <h3 className="category-label">Recently Added</h3>
+      <div className="recently-added-box">
+        <div className="card-box">
+          <img src="./img/1 (3).png" alt="" />
+          <span>P Name</span>
+          <span>P Price</span>
+        </div>
+        <div className="card-box">
+          <img src="./img/1 (6).png" alt="" />
+          <span>P Name</span>
+          <span>P Price</span>
+        </div>
+        <div className="card-box">
+          <img src="./img/1 (4).png" alt="" />
+          <span>P Name</span>
+          <span>P Price</span>
+        </div>
+        <div className="card-box">
+          <img src="./img/1 (1).png" alt="" />
+          <span>P Name</span>
+          <span>P Price</span>
+        </div>
+        <div className="card-box">
+          <img src="./img/1 (2).png" alt="" />
+          <span>P Name</span>
+          <span>P Price</span>
         </div>
       </div>
     </>
