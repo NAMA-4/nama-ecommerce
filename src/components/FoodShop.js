013764b86d82d74d3d7e2f22405db4a16776e07c
lijar <beljar1214@gmail.com>
2022-04-29 11:39:56 +0630

    'ad'

--- src/components/FoodShop.js
+++ src/components/FoodShop.js
@@ -8,7 +8,79 @@ const FoodShop = () => {
     <div>
       <ImagesliderFood />
       <Category />
+      <RecentlyAdded />
+      <EditorChoice />
     </div>
+  )
+}
+
+const RecentlyAdded = () => {
+  return (
+    <>
+      <h3 className="category-label">Recently Added</h3>
+      <div className="recently-added-box">
+        <div className="card-box">
+          <img src="./img/9 (1).jpg" alt="" />
+          <span>P Name</span>
+          <span>P Price</span>
+        </div>
+        <div className="card-box">
+          <img src="./img/9 (2).jpg" alt="" />
+          <span>P Name</span>
+          <span>P Price</span>
+        </div>
+        <div className="card-box">
+          <img src="./img/9 (3).jpg" alt="" />
+          <span>P Name</span>
+          <span>P Price</span>
+        </div>
+        <div className="card-box">
+          <img src="./img/15 (8).jpg" alt="" />
+          <span>P Name</span>
+          <span>P Price</span>
+        </div>
+        <div className="card-box">
+          <img src="./img/15 (9).jpg" alt="" />
+          <span>P Name</span>
+          <span>P Price</span>
+        </div>
+      </div>
+    </>
+  )
+}
+
+const EditorChoice = () => {
+  return (
+    <>
+      <h3 className="category-label">EditorChoice</h3>
+      <div className="recently-added-box">
+        <div className="card-box">
+          <img src="./img/9 (1).jpg" alt="" />
+          <span>P Name</span>
+          <span>P Price</span>
+        </div>
+        <div className="card-box">
+          <img src="./img/9 (2).jpg" alt="" />
+          <span>P Name</span>
+          <span>P Price</span>
+        </div>
+        <div className="card-box">
+          <img src="./img/9 (3).jpg" alt="" />
+          <span>P Name</span>
+          <span>P Price</span>
+        </div>
+        <div className="card-box">
+          <img src="./img/15 (8).jpg" alt="" />
+          <span>P Name</span>
+          <span>P Price</span>
+        </div>
+        <div className="card-box">
+          <img src="./img/15 (9).jpg" alt="" />
+          <span>P Name</span>
+          <span>P Price</span>
+        </div>
+      </div>
+    </>
   )
 }
 
