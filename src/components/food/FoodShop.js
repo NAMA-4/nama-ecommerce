64af584bc44298d478bba7d2cbed89369746816e
lijar <beljar1214@gmail.com>
2022-04-29 18:40:09 +0630

    'ad'

--- src/components/food/FoodShop.js
+++ src/components/food/FoodShop.js
@@ -1,15 +1,12 @@
 import * as React from 'react'
 import { styled } from '@mui/material/styles'
 import Card from '@mui/material/Card'
-import CardHeader from '@mui/material/CardHeader'
 import CardMedia from '@mui/material/CardMedia'
 import CardContent from '@mui/material/CardContent'
 import CardActions from '@mui/material/CardActions'
 import Collapse from '@mui/material/Collapse'
-import Avatar from '@mui/material/Avatar'
 import IconButton from '@mui/material/IconButton'
 import Typography from '@mui/material/Typography'
-import { red } from '@mui/material/colors'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 
 const ExpandMore = styled((props) => {
@@ -32,23 +29,16 @@ export default function FoodShop() {
 
   return (
     <Card sx={{ maxWidth: 400 }}>
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            <img style={{ width: '3rem' }} src="../img/shop.jpg" alt="" />
-          </Avatar>
-        }
-        title="Julie မာလာရှမ်းကော"
-      />
       <CardMedia
         component="img"
-        height="200"
+        height="300"
         image="../img/shop.jpg"
         alt="Paella dish"
       />
       <CardContent>
         <Typography variant="body2" color="text.secondary">
-          ဆိုင်ဖွင့်ချိန် | မနက် ၉ - ည ၈
+          <div>Julie မာလာရှမ်းကော</div>
+          <div>ဆိုင်ဖွင့်ချိန် | မနက် ၉ - ည ၈</div>
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
